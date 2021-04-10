8b1cc0311dc440fcb443ad37a10d2490c90da606
bazinga94 <bazinga940@gmail.com>
2021-04-10 13:01:17 +0900

    Network Unit test 프로젝트 추가

---
+++ NetworkUnitTest/NetworkUnitTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NetworkUnitTest
+//
+//  Created by Jongho Lee on 2021/04/10.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
