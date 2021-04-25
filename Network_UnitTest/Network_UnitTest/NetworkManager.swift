d6455c53a41e0d63b3f7fa2b37c5d7232d2d4d06
Jongho Lee <bazinga940@gmail.com>
2021-04-25 13:55:39 +0900

    필요없는 주석 제거 + Marvel Model 추가

--- Network_UnitTest/Network_UnitTest/NetworkManager.swift
+++ Network_UnitTest/Network_UnitTest/NetworkManager.swift
@@ -7,6 +7,7 @@
 import Foundation
 
 // 우아한 형제들 기술블로그 참고 https://woowabros.github.io/swift/2020/12/20/ios-networking-and-testing.html
+// Marvel API 참고 https://medium.com/makingtuenti/writing-a-scalable-api-client-in-swift-4-b3c6f7f3f3fb
 
 protocol URLSessionProtocol {
 	func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
@@ -102,63 +103,4 @@ extension NetworkProtocol {
 			}
 		task.resume()
 	}
-
-//	init(session: URLSessionProtocol = URLSession.shared) {
-//		self.session = session
-//	}	// Class가 아니라 Protocol로 사용
-
-//	func fetchRequest<T: Decodable>(url: URL, type: HttpMethod, body: String? = nil, completion: @escaping (Result<T, APIError>) -> Void) {
-//		var request = URLRequest(url: url)
-//		request.httpMethod = type.rawValue
-//		request.httpBody = body?.data(using: .utf8)
-//
-//		let task: URLSessionDataTask = session
-//			.dataTask(with: request) { data, urlResponse, error in
-//				guard let response = urlResponse as? HTTPURLResponse,
-//					  (200...399).contains(response.statusCode) else {
-//					completion(.failure(.httpStatus))
-//					return
-//				}
-//
-//				guard let data = data else {
-//					return completion(.failure(.dataNil))
-//				}
-//
-//				guard let model = try? JSONDecoder().decode(T.self, from: data) else {
-//					completion(.failure(.decodingJSON))
-//					return
-//				}
-//				completion(.success(model))
-//			}
-//		task.resume()
-//	}
 }
-
-//class NetworkManager {
-//
-//	func fetchRequest<T: Decodable>(url: URL, type: HttpMethod, body: String? = nil, completion: @escaping (Result<T, APIError>) -> Void) {
-//		var request = URLRequest(url: url)
-//		request.httpMethod = type.rawValue
-//		request.httpBody = body?.data(using: .utf8)
-//
-//		let task: URLSessionDataTask = session
-//			.dataTask(with: request) { data, urlResponse, error in
-//				guard let response = urlResponse as? HTTPURLResponse,
-//					  (200...399).contains(response.statusCode) else {
-//					completion(.failure(.httpStatus))
-//					return
-//				}
-//
-//				guard let data = data else {
-//					return completion(.failure(.dataNil))
-//				}
-//
-//				guard let model = try? JSONDecoder().decode(T.self, from: data) else {
-//					completion(.failure(.decodingJSON))
-//					return
-//				}
-//				completion(.success(model))
-//			}
-//		task.resume()
-//	}	// 왜 에러가 생기지?!
-//}
